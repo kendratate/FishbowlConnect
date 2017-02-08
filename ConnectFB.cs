1726a80900a11e26953f7aa47d91342e56759332
Kendra Tate <kendra@brandowconsulting.com>
2017-02-08 08:42:50 -0700

    First Commit

---
+++ ConnectFB.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.Xml;
+using System.IO;
+using System.Threading;
+
+namespace ConnectFB
+{
+    class Program
+    {
+        public static void FBODBC()
+        {
+            Console.Write("inside FBODBC");
+            String key = "";
+
+            // XML login string
+            String loginCommand = createLoginXml("admin", "admin");
+            Console.Write("Client Started... \n");
+            ConnectionObject connectionObject = new ConnectionObject();
+
+            //Send Login Command once to get fishbowl server to recognize the connection attempt
+            //or pull the key off the line if already connected
+            key = pullKey(connectionObject.sendCommand(loginCommand));
+            if (key == "null")
+            {
+                Console.Write("Please accept the connection attempt on the fisbowl server and press return");
+                Console.ReadLine();
+                key = pullKey(connectionObject.sendCommand(loginCommand));
+            }
+
+            //Now create the query to pull data out of fishbowl using the key
+            //String customerNameList = connectionObject.sendCommand(listCustomerName(key));
+            //Console.Write(customerNameList);
+
+            //this call work fine
+            String partList = connectionObject.sendCommand(listPartList(key));
+            Console.Write(partList);
+
+            //this ExecuteQueryRq doesn't work: throws "This stream does not support seek operations"
+            //String inventoryList = connectionObject.sendCommand(listInventory(key));
+            String inventoryList = connectionObject.sendCommand("<ExecuteQueryRq><Query>SELECT * from company</Query></ExecuteQueryRq>");
+            Console.Write(inventoryList);
+
+            //Added to keep the cmd screen open
+            Console.ReadLine();
+
+        }
+
+        private static String createLoginXml(string username, string password)
+        {
+            System.Text.StringBuilder buffer = new System.Text.StringBuilder("<FbiXml><Ticket/><FbiMsgsRq><LoginRq><IAID>");
+            buffer.Append("222");
+            buffer.Append("</IAID><IAName>");
+            buffer.Append("Inventory Reconcile");
+            buffer.Append("</IAName><IADescription>");
+            buffer.Append("Fishbowl/Quickbooks Inventory Reconcile");
+            buffer.Append("</IADescription><UserName>");
+            buffer.Append(username);
+            buffer.Append("</UserName><UserPassword>");
+
+            MD5 md5 = MD5CryptoServiceProvider.Create();
+            byte[] encoded = md5.ComputeHash(System.Text.Encoding.ASCII.GetBytes(password));
+            string encrypted = Convert.ToBase64String(encoded, 0, 16);
+            buffer.Append(encrypted);
+            buffer.Append("</UserPassword></LoginRq></FbiMsgsRq></FbiXml>");
+
+            return buffer.ToString();
+        }
+
+        //Pull the session Key out of the server response string
+        private static String pullKey(String connection)
+        {
+            String key = "";
+            using (XmlReader reader = XmlReader.Create(new StringReader(connection)))
+            {
+                while (reader.Read())
+                {
+                    //if (reader.NodeType == XmlNodeType.Element && reader.Name.Equals("Key"))
+                    if (reader.Name.Equals("Key") && reader.Read())
+                    {
+                        return reader.Value.ToString();
+                    }
+                }
+            }
+            return key;
+        }
+
+
+        // The following generates different querries 
+        private static string listCustomerName(string key)
+        {
+            return "<FbiXml><Ticket><Key>" + key + "</Key></Ticket><FbiMsgsRq><CustomerNameListRq></CustomerNameListRq></FbiMsgsRq></FbiXml>";
+        }
+
+        private static string listPartList(string key)
+        {
+            return "<FbiXml><Ticket><Key>" + key + "</Key></Ticket><FbiMsgsRq><InvQtyRq><PartNum>B202</PartNum><LastModifiedFrom></LastModifiedFrom><LastModifiedTo></LastModifiedTo></InvQtyRq></FbiMsgsRq></FbiXml>";
+        }
+
+        private static string listInventory(string key)
+        {
+            //return "<ExecuteQueryRq><Query>SELECT part.num AS part, part.description, uom.code AS uomcode,COALESCE((SELECT SUM(qtyonhand.qty) FROM qtyonhand WHERE qtyonhand.partid = part.id), 0) AS qty FROM part LEFT JOIN uom ON part.uomid = uom.id</Query></ExecuteQueryRq>";
+            return "<ExecuteQueryRq><Query>SELECT * from company</Query></ExecuteQueryRq>";
+
+        }
+    }
+}
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static ConnectFB.Program;
+
+namespace QuickbooksFishbowl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void createReport_Click(object sender, RoutedEventArgs e)
+        {
+
+            Console.WriteLine("I am in the report writing function");
+
+            FBODBC();
+        }
+
+        private void chooseFishbowl_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".FDB";
+            dlg.Filter = "Fishbowl Database (*.FDB)|*.FDB";
+            dlg.Title = "Select Fishbowl Database";
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                fishbowlDatabase.Text = filename;
+            }
+        }
+
+        private void chooseQuickbooks_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".QBW";
+            dlg.Filter = "Quickbooks Company File (*.qbw)|*.QBW";
+            dlg.Title = "Select Quickbooks Data File";
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                quickbooksDatabase.Text = filename;
+            }
+        }
+        private void chooseExcelFile_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".xls";
+            dlg.Filter = "Excel Report File and Location (*.xls)|*.XLS";
+            dlg.Title = "Select Output Excel File";
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                excelFile.Text = filename;
+            }
+        }
+
+    }
+    }
+
+
+
